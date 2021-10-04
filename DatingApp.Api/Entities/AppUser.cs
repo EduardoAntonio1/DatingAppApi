0969a2fededfe75be93a83932d601ae3c4d0cdae
Eduardo Antonio Zamora Hernandez <ezamora@hermescorp.io>
2021-10-04 08:05:38 -0500

    datetime

--- DatingApp.Api/Entities/AppUser.cs
+++ DatingApp.Api/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatingApp.Api.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,5 +14,32 @@ namespace DatingApp.Api.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime Created {get; set;}
+
+        public DateTime LastActive {get; set;}
+
+        public string Gender {get; set;}
+
+        public string Introduction {get; set;}
+
+        public string LookingFor {get; set;}
+
+        public string Interests {get; set;}
+
+        public string City {get; set;}
+
+        public string Country {get; set;}
+
+        public ICollection<Photo> MyProperty {get; set;}
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
