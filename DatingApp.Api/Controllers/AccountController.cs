6bad06a0f0059cf19cc45f59018e1abd9618035f
Eduardo Antonio Zamora Hernandez <ezamora@hermescorp.io>
2021-11-18 12:32:34 -0600

    last

--- DatingApp.Api/Controllers/AccountController.cs
+++ DatingApp.Api/Controllers/AccountController.cs
@@ -1,41 +1,56 @@
-﻿using DatingApp.Api.Data;
+﻿using AutoMapper;
 using DatingApp.Api.DTOs;
 using DatingApp.Api.Entities;
 using DatingApp.Api.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.Api.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ITokenService tokenService,
+            IMapper mapper)
         {
-            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
             _tokenService = tokenService;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Username)) return BadRequest("Username is already taken");
+            if (string.IsNullOrEmpty(registerDto.Username)) return BadRequest("User Name is already Taken");
+            if (await UserExists(registerDto.Username)) return BadRequest("User Name is already Taken");
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower()
-            };
+            var user = _mapper.Map<AppUser>(registerDto);
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            user.UserName = registerDto.Username.ToLower();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+            if (!roleResult.Succeeded) return BadRequest("Failed to add user to role: " + result.Errors);
 
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -44,16 +59,21 @@ namespace DatingApp.Api.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
-                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _userManager.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username or password");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
 
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
-                // PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                Token = await _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -63,7 +83,7 @@ namespace DatingApp.Api.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
         #endregion
---
+++ DatingAppUaa.UnitTests/Pruebas/AccountControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using DatingApp.Api.DTOs;
+using DatingAppUaa.UnitTests.Helpers;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatingAppUaa.UnitTests.Pruebas
+{
+    public class AccountControllerTests
+    {
+        private string apiRoute = "api/account";
+        private readonly HttpClient _client;
+        private HttpResponseMessage HttpResponse;
+        private string requestUri;
+        private string registeredObject;
+        private HttpContent httpContent;
+        public AccountControllerTests()
+        {
+            _client = TestHelper.Instance.Client;
+        }
+
+        [Theory]
+        [InlineData("BadRequest", "", "KnownAs", "Gender", "2000-01-01", "City", "Country", "Password")]
+        [InlineData("BadRequest", "lisa", "KnownAs", "Gender", "2000-01-01", "City", "Country", "Password")]
+        public async Task Register_ShouldFail(string statusCode, string username, string knownAs, string gender, DateTime dateOfBirth, string city, string country, string password)
+        {
+
+        }
+
+
+        #region Privated methods
+        private static string GetRegisterObject(RegisterDto registerDto)
+        {
+            var entityObject = new JObject()
+            {
+                { nameof(registerDto.Username), registerDto.Username },
+                { nameof(registerDto.KnownAs), registerDto.KnownAs },
+                { nameof(registerDto.Gender), registerDto.Gender },
+                { nameof(registerDto.DateOfBirth), registerDto.DateOfBirth },
+                { nameof(registerDto.City), registerDto.City },
+                { nameof(registerDto.Country), registerDto.Country },
+                { nameof(registerDto.Password), registerDto.Password }
+            };
+
+            return entityObject.ToString();
+        }
+
+        private StringContent GetHttpContent(string entity)
+        {
+            return new StringContent(entity, Encoding.UTF8, "application/json");
+        }
+
+        #endregion
+    }
+}
